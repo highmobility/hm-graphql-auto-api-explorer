d3a0992f64b8033ded9b6229103f20db7d14cb46
Kaarel Õun <kaareloun@gmail.com>
2021-08-06 14:43:13 +0300

    dropdown animations

--- client/src/components/Dropdown.js
+++ client/src/components/Dropdown.js
@@ -1,32 +1,57 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import '../styles/Dropdown.scss'
 import { ReactComponent as ChevronIcon } from '../images/chevron.svg'
+import { useClickAway } from 'react-use'
+import { animated, useTransition } from '@react-spring/web'
 
 export default function Dropdown({ className, label, value, items = [] }) {
   const [open, setOpen] = useState(false)
+  const ref = useRef(null)
+  useClickAway(ref, () => {
+    setOpen(false)
+  })
 
   const onClickItem = (item) => {
     item.onClick()
     setOpen(false)
   }
 
+  const transitions = useTransition(open ? [open] : [], {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  })
+
   return (
-    <div className={`Dropdown ${className || ''} ${open ? 'Open' : ''}`}>
+    <div
+      className={`Dropdown ${className || ''} ${open ? 'Open' : ''}`}
+      ref={ref}
+    >
       <div className="DropdownButton" onClick={() => setOpen(!open)}>
         <span>{label}</span>
         <ChevronIcon />
       </div>
-      <div className={`DropdownContent`}>
-        {items.map((item, key) => (
-          <div
-            className={`DropdownItem ${value === item.value ? 'Selected' : ''}`}
-            key={key}
-            onClick={() => onClickItem(item)}
-          >
-            {item.label}
+      {transitions(({ opacity }) => (
+        <animated.div
+          style={{
+            opacity,
+          }}
+        >
+          <div className={`DropdownContent`}>
+            {items.map((item, key) => (
+              <div
+                className={`DropdownItem ${
+                  value === item.value ? 'Selected' : ''
+                }`}
+                key={key}
+                onClick={() => onClickItem(item)}
+              >
+                {item.label}
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </animated.div>
+      ))}
     </div>
   )
 }
