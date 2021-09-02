653758dc81d202507d52e8b3f37ef995b3ead316
Kaarel Õun <kaareloun@gmail.com>
2021-09-02 18:22:15 +0300

    fix google map error

--- client/src/components/GoogleMap.js
+++ client/src/components/GoogleMap.js
@@ -69,7 +69,7 @@ const GoogleMap = ({
 
         newInstance.addListener('tilesloaded', () => {
           setTilesLoaded(true)
-          if (domRef.current.children.length > 1) {
+          if (domRef?.current?.children?.length > 1) {
             setHasError(true)
           }
         })
